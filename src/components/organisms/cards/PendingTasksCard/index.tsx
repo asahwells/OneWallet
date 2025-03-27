7314e8b2057e594ab2fa326befb2f94a05507886
Emmanuel Umeh <Emmanuel@abacus.co>
2025-03-27 15:17:32 +0100

    feat: empty state for dashboard

--- src/components/organisms/cards/PendingTasksCard/index.tsx
+++ src/components/organisms/cards/PendingTasksCard/index.tsx
@@ -1,8 +1,39 @@
 import {HStack, Stack, Text, VStack} from "@chakra-ui/react";
 import {PendingTasksCardProps} from "../interfaces";
+import EmptyTaskIcon from "../../../atoms/icons/EmptyTasksIcon";
 
 const PendingTasksCard = ({pendingTasks}: PendingTasksCardProps) => {
 
+
+    const getPendingTasksList= () => {
+        if(!pendingTasks?.length){
+            return (
+                <Stack py={16} spacing={4} w={'full'} alignItems={'center'} justifyContent={'center'}>
+                    <EmptyTaskIcon />
+
+                    <Text fontSize={'16px'} fontWeight={'500'}>
+                        You do no have any pending task at this time
+                    </Text>
+                </Stack>
+            )
+        }
+
+        return (
+            <>
+
+                {pendingTasks.map((task, index) => {
+                    const isLast = index === pendingTasks.length - 1
+                    return (
+                        <VStack alignItems={'start'} w={'full'} px={5} py={4} spacing={2} justifyContent={'space-between'} borderBottom={isLast? '' : '1px solid #DADDE2'} key={index}>
+                            <Text fontWeight={'400'} fontSize={'16px'} letterSpacing={'-1.2%'} color={'#344256'}>{task.title}</Text>
+                            <Text fontWeight={'400'} fontSize={'14px'} letterSpacing={'-1%'} color={'#344256'}>Due Date: {task.dueDate}</Text>
+                        </VStack>
+                    )
+                })
+                }</>
+        )
+
+    }
     return (
         <Stack bg={'white'} w={'full'} borderRadius={'8px'} border={'0.37px solid #E2E8F0'}>
 
@@ -13,16 +44,9 @@ const PendingTasksCard = ({pendingTasks}: PendingTasksCardProps) => {
             </HStack>
 
           <Stack w={'full'}>
-              {pendingTasks.map((task, index) => {
-                  const isLast = index === pendingTasks.length - 1
-                  return (
-                      <VStack alignItems={'start'} w={'full'} px={5} py={4} spacing={2} justifyContent={'space-between'} borderBottom={isLast? '' : '1px solid #DADDE2'} key={index}>
-                          <Text fontWeight={'400'} fontSize={'16px'} letterSpacing={'-1.2%'} color={'#344256'}>{task.title}</Text>
-                          <Text fontWeight={'400'} fontSize={'14px'} letterSpacing={'-1%'} color={'#344256'}>Due Date: {task.dueDate}</Text>
-                      </VStack>
-                  )
-              })
-              }
+
+              {getPendingTasksList()}
+
           </Stack>
 
         </Stack>
