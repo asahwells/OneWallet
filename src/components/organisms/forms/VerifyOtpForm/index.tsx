fb197987f76ab8f6a8e26f9bdf58840b27b9b967
CHIBUIKE ILO <82292997+WESTIEWEB@users.noreply.github.com>
2025-03-28 09:04:45 +0100

    fix: corrected verify otp form butto to match ui design

--- src/components/organisms/forms/VerifyOtpForm/index.tsx
+++ src/components/organisms/forms/VerifyOtpForm/index.tsx
@@ -84,7 +84,7 @@ import {
                 text={'Verify'}
                 onClick={handleClick}
                 borderRadius={"8px"}
-                h={"152px"}
+                h={"56px"}
                 sx={{
                   backgroundColor: otp.length < 4 ? "#CFDADC" : "#0F454F",
                   color: otp.length < 4 ? "#171C23" : "#FCFCFC",
