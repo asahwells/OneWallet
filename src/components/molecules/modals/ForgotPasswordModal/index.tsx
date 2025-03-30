cb2dcc52e1eca3bced9eb34138a896c27b823770
CHIBUIKE ILO <82292997+WESTIEWEB@users.noreply.github.com>
2025-03-30 18:10:53 +0100

    feat:Forgot-password-modal update

--- src/components/molecules/modals/ForgotPasswordModal/index.tsx
+++ src/components/molecules/modals/ForgotPasswordModal/index.tsx
@@ -10,6 +10,7 @@ import {
 } from '@chakra-ui/react';
 import ForgotPasswordIcon from 'components/atoms/icons/forgotPasswordIcon';
 import { IForgotPasswordModalProps } from '../interfaces';
+import BaseButton from 'components/molecules/buttons/BaseButton';
 
 const ForgotPasswordModal = ({
   isOpen,
@@ -23,16 +24,28 @@ const ForgotPasswordModal = ({
       <ModalContent 
         {...props} 
       >
-        <ModalCloseButton />
+        <ModalCloseButton display={{base: 'inline-block', md: 'none'}} />
         <ModalBody>
-          <VStack spacing={4}>
-            <ForgotPasswordIcon />
-            <Text fontSize="40px" fontWeight={500} lineHeight="48px" letterSpacing="-2.8%" color="#344256">
+          <VStack alignItems={{base:'flex-start', md: 'center'}} position={'relative'} spacing={4}>
+            <ForgotPasswordIcon  />
+            <Text fontSize="16px" fontWeight={500} lineHeight="24.63px" letterSpacing="-1.2%" color="#344256">
               Forgot Password?
             </Text>
-            <Text fontSize="20px" fontWeight={400} lineHeight="32px" letterSpacing="-1.4%" color="#344256">
+            <Text fontSize="14px" fontWeight={400} lineHeight="22px" letterSpacing="-1%" color="#222B38">
               Please contact Admin
             </Text>
+
+            <BaseButton
+              variant={"ghost"}
+              text={'Okay'}
+              onClick={() => onClose()}
+              backgroundColor="#FFFFFF"
+              color="#0F454F"
+              borderRadius={"8px"}
+              border={'1.2px solid #0F454F'}
+              h={"56px"}
+              w={{base: '100%', md: '520px'}}
+              />
           </VStack>
         </ModalBody>
       </ModalContent>
