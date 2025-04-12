ae800e0d792c45c276123b29248a81c34eb5ec83
Day-veed <davidadekunle9@gmail.com>
2025-04-12 12:12:22 +0100

    fix: dashboard ui fix

--- src/components/organisms/cards/QuickActionCard/index.tsx
+++ src/components/organisms/cards/QuickActionCard/index.tsx
@@ -78,7 +78,7 @@ const QuickActionCard = () => {
                         </Stack>
 
                         <Box w={'full'}>
-                            <BaseButton variant={'outline'} bg={'#FFFFFF'} w={'full'}  text={'Download Report'}/>
+                            <BaseButton variant={'outline'} bg={'#FFFFFF' } w={'full'}  text={'Download Report'}/>
                         </Box>
 
                     </VStack>
