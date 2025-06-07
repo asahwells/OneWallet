9a78f1f2bf3feb8ab8e3d1e8fbd17aa7269dc3d0
Asah Victor <52835749+asahwells@users.noreply.github.com>
2025-06-07 18:10:12 +0100

    Updated http.ts

--- src/api-services/http.ts
+++ src/api-services/http.ts
@@ -13,6 +13,7 @@ export const BASE_AXIOS = axios.create({
 
 const requestConfig = (config: AxiosRequestConfig) => {
     const token = Cookies.get(StorageToken);
+  console.log(process.env.NEXT_PUBLIC_BASE_URL)
 
     return {
         ...config,
